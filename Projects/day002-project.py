158dcd0e3b6f6935115e65d8a4662f04961082f9
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-25 10:13:54 +0530

    change code

--- Projects/day002-project.py
+++ Projects/day002-project.py
@@ -1,7 +1,7 @@
 #get user inputs and assign them into variables
 
 name=input("Enter your name: ")
-age = int(input("Enter your age: "))
+age = input("Enter your age: ")
 hobby = input("Enter your hobby: ")
 
-print("Hello my name is "+ name + ". I am " + str(age) + " years old. I love " + hobby)
+print("Hello my name is "+ name + ". I am " + age + " years old. I love " + hobby)
