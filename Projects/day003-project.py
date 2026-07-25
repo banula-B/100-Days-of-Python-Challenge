3a7cb142ee0de0222b79c8347d53f6834d81fe3b
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-26 00:38:30 +0530

    feat: add simple calculator script for day 3 project

---
+++ Projects/day003-project.py
@@ -0,0 +1,12 @@
+#this is simple calculator with fixed operations
+
+a = float(input("Enter a number: "))
+b = float(input("Enter a number: "))
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
+print("Floor Division: ", a // b)
+print("Modulo: ", a % b)
+print("Exponent: ", a ** b)
