ed1ebb34bafc1fc23ba71ec551d3b552605dcba6
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-26 10:36:03 +0530

    feat: add day003 exercises

---
+++ Exercises/day003_Ex1.py
@@ -0,0 +1,8 @@
+'''Calculate the remainder when **10** is divided by **3** using 
+the modulo operator.
+'''
+
+
+remainder = 10 % 3
+print("Remainder: ", remainder)
+
---
+++ Exercises/day003_Ex2.py
@@ -0,0 +1,9 @@
+'''Write a program that calculates the area of a rectangle.
+Formula: Area = Length × Width
+'''
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+print("Area: ", area)
