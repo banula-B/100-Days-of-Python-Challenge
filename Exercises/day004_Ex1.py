567a2c35ecdd8afd53614b31c36c6bb11013decf
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-27 06:38:21 +0530

    feat: add day 004 exercise 1

---
+++ Exercises/day004_Ex1.py
@@ -0,0 +1,7 @@
+'''this is a simple program to take two nummbers from user 
+ and return their sum'''
+ 
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+print("The sum of", num1, "and", num2, "is", num1 + num2)
