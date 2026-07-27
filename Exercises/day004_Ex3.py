dd369067ef3a885c124b40f62e8295375f6030dc
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-27 06:56:08 +0530

    feat: add day 004 exercise 3

---
+++ Exercises/day004_Ex3.py
@@ -0,0 +1,12 @@
+#this is age calculator
+
+#take user birth year
+birth_year = int(input("Enter Your Birth Year: "))
+
+#calculate age
+age = 2026 -birth_year
+
+#print age
+print("You are", age, "years old.")
+
+
