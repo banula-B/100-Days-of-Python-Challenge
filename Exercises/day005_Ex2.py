a0cbee2a83094675f7a369efe037de6d5f55c2b1
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-28 06:39:09 +0530

    feat: add day 005 exercises

---
+++ Exercises/day005_Ex2.py
@@ -0,0 +1,10 @@
+#This is a simple exercise to check if a password is correct or not
+
+password = input("Enter your password: ")
+
+if password == "python123":
+    print("Access Granted")
+    
+else:
+    print("Access Denied")
+    
---
+++ Exercises/day005_Ex3.py
@@ -0,0 +1,9 @@
+#This is a simple exercise to check if you are the Admin or not
+
+name = input("Enter your name: ")
+
+if name == "Admin":
+    print("Welcome, System Administrator.")
+else:
+    print("Access Denied")
+    
