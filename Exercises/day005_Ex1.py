90ac7cb68edbf15b270937d3416055cf27c63165
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-28 06:36:14 +0530

    feat: add day005 exercise 1

---
+++ Exercises/day005_Ex1.py
@@ -0,0 +1,9 @@
+#This is a simple exercise to check if a number is positive or negative
+
+n= int(input("Enter any Number: "))
+
+if n > 0:
+    print("Positive!")
+else:
+    print("Negative!")
+    
