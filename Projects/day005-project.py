b3027edbb03460fb647d8782887c01ca49667415
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-28 06:31:40 +0530

    feat: add  day 005 project

---
+++ Projects/day005-project.py
@@ -0,0 +1,8 @@
+# This is a simple age checker using if statements 
+
+age = int(input("Enter your age: "))
+
+if age >= 13:
+    print("You are old enough to watch a PG-13 movie!")
+else:
+    print("You must be at least 13 years old to watch this movie")
