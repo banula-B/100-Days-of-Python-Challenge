e77789dd283fc04953d4da41d624538b6795d651
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-29 07:16:50 +0530

    feat: add day006 exercise

---
+++ Exercises/day006_Ex2.py
@@ -0,0 +1,11 @@
+#this is a simple project to check if the password is correct or not
+#the password is "secret"
+
+password = input("Enter Your Password: ")
+
+if password == "secret":
+    print("Welcome")
+else:
+    print("Wrong Password")
+
+    
