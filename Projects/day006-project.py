5cfca94dbbf5d04a90d1840910caf3cf88fd9e4c
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-29 07:04:29 +0530

    feat: add day 006 mini project

---
+++ Projects/day006-project.py
@@ -0,0 +1,10 @@
+# This is a simple project to find if a number is even or odd
+
+num = int(input("Enter Your Number: "))
+
+if num % 2 == 0:
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
+
+    
