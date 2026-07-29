b11f6d1b0daad06d1f412f2b8d6e8e60ae418e43
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-29 07:16:02 +0530

    feat: add day006 exercise

---
+++ Exercises/day006_Ex1.py
@@ -0,0 +1,10 @@
+# This is a simple pass fail checker using if else statement
+# If your marks are higher or equal to 50 then you will pass else you will fail
+
+a = int(input("Enter Your Marks: "))
+
+if a >= 50:
+    print("You Passed")
+else:
+    print("You Failed")
+    
