a8502e0efc246bd82af6a68461079d56295900fa
Banula Bimsara <banu44bimsara@gmail.com>
2026-07-29 07:17:38 +0530

    feat: add day006 exercise

---
+++ Exercises/day006_Ex3.py
@@ -0,0 +1,8 @@
+# This is a simple priject to check, if a number is positive or negative
+
+number = int(input("Enter Your Number: "))
+
+if number >= 0:
+    print("The Number is Positive")
+else:
+    print("The Number is Negative")
