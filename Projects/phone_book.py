1ebffca89e01dc04a67318b7d40eec2dc4c67ee6
Banula Bimsara <banu44bimsara@gmail.com>
2026-08-10 23:45:39 +0530

    feat: add day 18 mini project

---
+++ Projects/phone_book.py
@@ -0,0 +1,36 @@
+# This is a phone book using dictionary
+
+phone_book={}
+
+
+
+while True:
+    print("\nWelcome to Digital Phone Book")
+    print("1. Add Contact")
+    print("2. Search Contact")
+    print("3. View All")
+    print("4. Exit")
+
+    choice = int(input("Enter your selection: "))
+
+    if choice ==1:
+        name = input("Enter Name: ").strip().title()
+        phone_number = int(input("Enter Number: "))
+        phone_book[name] = phone_number
+        print(f"Contact {name} saved!")
+
+    elif choice == 2:
+        name = input("Enter name: ").strip().title()
+        print(f"Number: {phone_book.get(name, 'Not Found')}")
+
+    elif choice == 3:
+        print("\n All Contacts")
+        for name,phone_number in phone_book.items():
+            print(f"{name} : {phone_number}")
+
+    elif choice == 4:
+        break
+
+
+
+    
