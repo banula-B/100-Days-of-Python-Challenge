b957fbd2ecd7649b4968599590cb97068f537e58
Banula Bimsara <banu44bimsara@gmail.com>
2026-08-13 08:54:54 +0530

    feat: add day 21 mini project phase 2

--- Projects/func_calculator.py
+++ Projects/func_calculator.py
@@ -4,8 +4,8 @@ print("Calculator")
 print("")
 print("1. Addition")
 print("2. Subtraction")
-# print("3. Multiplication")
-# print("4. Division")
+print("3. Multiplication")
+print("4. Division")
 print("")
 
 choice = int(input("Enter your choice (1-4): "))
@@ -18,20 +18,20 @@ def add(num1, num2):
 def sub(num1, num2):
     return num1 - num2
 
-# def mul(num1, num2):
-#     return num1 * num2
+def mul(num1, num2):
+    return num1 * num2
 
-# def div(num1, num2):
-#     return num1 / num2
+def div(num1, num2):
+    return num1 / num2
 
 if choice == 1:
     print("Sum of two numbers is: ", add(num1, num2))
 elif choice == 2:
     print("Difference of two numbers is: ", sub(num1, num2))
-# elif choice == 3:
-#     print("Product of two numbers is: ", mul(num1, num2))
-# elif choice == 4:
-#     print("Division of two numbers is: ", div(num1, num2))
+elif choice == 3:
+    print("Product of two numbers is: ", mul(num1, num2))
+elif choice == 4:
+    print("Division of two numbers is: ", div(num1, num2))
 else:
     print("Invalid choice")
 
