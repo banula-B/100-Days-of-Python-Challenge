cfbb327ed59a7cc149e31c00f19bb24da372e6ad
Banula Bimsara <banu44bimsara@gmail.com>
2026-08-19 08:25:52 +0530

    feat: add day 27 mini project

---
+++ Projects/even_filter.py
@@ -0,0 +1,8 @@
+def is_even(n):
+    return n % 2 == 0
+
+num = [1,2,3,4,5,6,7,8,9,10]
+
+even_numbers = list(filter(is_even, num))
+print(f"Original List: {num}")
+print(f"Even Numbers: {even_numbers}")
