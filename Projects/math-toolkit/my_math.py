ab15dd94e1a56b2a4fccdc3446722cb81348783b
Banula Bimsara <banu44bimsara@gmail.com>
2026-08-21 17:59:41 +0530

    feat: update day 29 mini project

--- Projects/math-toolkit/my_math.py
+++ Projects/math-toolkit/my_math.py
@@ -7,6 +7,9 @@ def is_prime(n):
     return True
 
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial(n - 1)
+    result = 1
+    
+    for i in range(1, n + 1):
+        result = result * i
+    
+    return result
